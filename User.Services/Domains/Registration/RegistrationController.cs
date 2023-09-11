f8e1b2f91462b052cf58627f4b8ea82de33c17b7
Daniel Whitney <whitneyd.dev@gmail.com>
2023-09-10 19:02:59 -0500

    setup to run swagger in development

---
+++ User.Services/Domains/Registration/RegistrationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace User.Services.Domains
+{
+    public class RegistrationController : ControllerBase
+    {
+        private readonly ILogger<RegistrationController> _logger;
+        public RegistrationController(ILogger<RegistrationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("/register")]
+        public async Task<IActionResult> RegisterUser()
+        {
+
+            return new ObjectResult("Ok") { StatusCode = 200 };
+        }
+    }
+}
+
